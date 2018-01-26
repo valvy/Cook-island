461cc526d566c34da85766bb3a9939a3a56b78f2
Quget <eddymeivogel@gmail.com>
2018-01-26 22:01:29 +0100

    Closes #6 movement done

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -20,28 +20,42 @@ public class Movement : MonoBehaviour
     {
 
 	}
-	
-	// Update is called once per frame
-	void Update ()
+    // Thank you internet
+    // https://forum.unity.com/threads/limiting-rotation-with-mathf-clamp.171294/
+    private float ClampAngle(float angle, float min, float max)
+    {
+        if (min < 0 && max > 0 && (angle > max || angle < min))
+        {
+            angle -= 360;
+            if (angle > max || angle < min)
+            {
+                if (Mathf.Abs(Mathf.DeltaAngle(angle, min)) < Mathf.Abs(Mathf.DeltaAngle(angle, max))) return min;
+                else return max;
+            }
+        }
+        else if (min > 0 && (angle > max || angle < min))
+        {
+            angle += 360;
+            if (angle > max || angle < min)
+            {
+                if (Mathf.Abs(Mathf.DeltaAngle(angle, min)) < Mathf.Abs(Mathf.DeltaAngle(angle, max))) return min;
+                else return max;
+            }
+        }
+
+        if (angle < min) return min;
+        else if (angle > max) return max;
+        else return angle;
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         transform.Translate(new Vector3(0,1,0) * speed * Time.deltaTime);
-
-
-        Debug.Log(transform.eulerAngles);
-        /*
-        if(transform.eulerAngles.z < range && rotation > 0)
-        {
-            transform.Rotate(new Vector3(0, 0, rotation * rotationSpeed * Time.deltaTime));
-        }
-        else if(transform.eulerAngles.z > (360-range) && rotation < 0)
-        {
-            transform.Rotate(new Vector3(0, 0, rotation * rotationSpeed * Time.deltaTime));
-        }
-        */
         transform.Rotate(new Vector3(0, 0, rotation * rotationSpeed * Time.deltaTime));
-
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, ClampAngle(transform.eulerAngles.z, -range, range));
+        
 		float horizontal = GyroInput.getInstance().getTilt();
-
         UpdateRotation(horizontal);
 
     }
