470d1f8d905dc3fb95a1371d2269674733f79576
Steffan <steffanvenema@hotmail.com>
2018-01-26 22:17:20 +0100

    Closes #19: Camera follow player, but not follow the rotation

---
+++ Assets/Scripts/FixedCameraRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCameraRotation : MonoBehaviour {
+    private Quaternion initialRotation;
+	// Use this for initialization
+	void Start () {
+        initialRotation = transform.rotation;
+	}
+
+    private void LateUpdate()
+    {
+        transform.rotation = initialRotation;
+    }
+}
