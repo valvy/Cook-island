b5d7f38601ca66408ce9a2f8b31eaea2a6225f29
Quget <eddymeivogel@gmail.com>
2018-01-27 20:35:30 +0100

    closes #32. added audio triggers, can now turn 360 degrees

---
+++ Assets/Scripts/StoryTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] storyClips;
+    private AudioSource audioSource;
+    private bool hit = false;
+	// Use this for initialization
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.clip = storyClips[Random.Range(0, storyClips.Length - 1)];
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public AudioClip GetSelectedAudioClip()
+    {
+        return audioSource.clip;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!hit)
+        {
+            audioSource.Play();
+            hit = true;
+        }
+    }
+}
