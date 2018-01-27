2aaabf68e00254a79ac90bd659c9b292f5e2dc8a
Steffan <steffanvenema@hotmail.com>
2018-01-27 12:36:46 +0100

    Closes #25: Create Startmenu

---
+++ Assets/Scripts/Menu/MenuButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+    public string StartScene;
+    public void StartGame()
+    {
+        StartCoroutine(LoadGameSceneAsync());
+    }
+
+    IEnumerator LoadGameSceneAsync()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(StartScene);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
